732a05106cc584276745cea8a21420e847d2db2a
adriaqf <adriaqf@gmail.com>
2024-02-18 00:55:07 +0100

    Cronometro cuadrado mantiene valores al cerrarse

--- app/src/main/java/com/easyfitness/ChronoDialogbox.java
+++ app/src/main/java/com/easyfitness/ChronoDialogbox.java
@@ -2,16 +2,20 @@ package com.easyfitness;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
+
 import gr.antoniom.chronometer.Chronometer;
 
 public class ChronoDialogbox extends Dialog implements
         android.view.View.OnClickListener {
-
+    public ChronoService chronoService;
     public Activity c;
     public Dialog d;
     public Button startstop, exit, reset;
@@ -21,19 +25,32 @@ public class ChronoDialogbox extends Dialog implements
     long stopTime = 0;
     private boolean chronoStarted = false;
     private boolean chronoResetted = false;
+    private Context mContext; // Asegúrate de tener una referencia al contexto
 
     public ChronoDialogbox(Activity a) {
         super(a);
         this.c = a;
     }
+    private void startChronoService() {
+        mContext = getContext();
+        Intent intent = new Intent(mContext,ChronoService.class);
+        mContext.startService(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.println("onCreateDialog");
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setTitle(c.getResources().getString(R.string.ChronometerLabel)); //ChronometerLabel
         setContentView(R.layout.dialog_chrono);
         this.setCanceledOnTouchOutside(false); // make it modal
+
+        startChronoService();
+        ChronoService chronoService = ChronoService.getInstance();
+        this.chronoService = chronoService;
+        System.out.println("valor timing al crear un nuevo dialog "+ chronoService.getTiming_start());
+
 
         startstop = findViewById(R.id.btn_startstop);
         exit = findViewById(R.id.btn_exit);
@@ -43,11 +60,30 @@ public class ChronoDialogbox extends Dialog implements
         startstop.setOnClickListener(this);
         exit.setOnClickListener(this);
         reset.setOnClickListener(this);
+        /*
+        startTime = SystemClock.elapsedRealtime();
         chrono.setBase(SystemClock.elapsedRealtime());
         chrono.start();
         startTime = SystemClock.elapsedRealtime();
         chronoStarted = true;
 
+        startstop.setText("Stop");*/
+
+        if(chronoService.getTiming_start() == 0){
+            startTime = SystemClock.elapsedRealtime();
+        }else{
+            startTime = chronoService.getTiming_start();
+            if( chronoService.getTiming_stop() == 0){
+                stopTime = SystemClock.elapsedRealtime();
+            }else{
+                stopTime = chronoService.getTiming_stop();
+            }
+            startTime = SystemClock.elapsedRealtime() - (stopTime - startTime);
+            chrono.setBase(startTime);
+        }
+
+        chrono.start();
+        chronoStarted = true;
         startstop.setText("Stop");
 
     }
@@ -59,6 +95,7 @@ public class ChronoDialogbox extends Dialog implements
                 if (chronoStarted) {
                     chrono.stop();
                     stopTime = SystemClock.elapsedRealtime();
+                    chronoService.setTiming_stop(stopTime);
                     chronoStarted = false;
                     startstop.setText("Start");
                 } else {
@@ -78,13 +115,16 @@ public class ChronoDialogbox extends Dialog implements
                 startTime = SystemClock.elapsedRealtime();
                 chrono.setBase(startTime);
                 chrono.setText("00:00:0");
+                System.out.println("btn_reset---------------------------");
                 chronoResetted = true;
                 break;
             case R.id.btn_exit:
-                chrono.stop();
+             /*   chrono.stop();
                 chronoStarted = false;
                 chrono.setText("00:00:0");
-                startstop.setText("Start");
+                startstop.setText("Start"); */
+                chronoService.setTiming_start(startTime);
+                chronoService.setTiming_stop(stopTime);
                 dismiss();
                 break;
             default:
---
+++ app/src/main/java/com/easyfitness/ChronoService.java
@@ -0,0 +1,64 @@
+package com.easyfitness;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class ChronoService extends Service {
+    private static ChronoService instance;
+    long timing_start = 0;
+    long timing_stop = 0;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Lógica para almacenar información aquí
+        System.out.println("onStartCommand");
+        intent = intent.putExtra("timing_start", timing_start);
+        intent = intent.putExtra("timing_stop", timing_stop);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        instance = null;
+    }
+
+    public long getTiming_start() {
+        return timing_start;
+    }
+
+    public void setTiming_start(long timing_start) {
+        this.timing_start = timing_start;
+    }
+    public long getTiming_stop() {
+        return timing_stop;
+    }
+    public void setTiming_stop(long timing_stop) {
+        this.timing_stop = timing_stop;
+    }
+    public void resetService(){
+        this.timing_start = 0;
+    }
+    public static ChronoService getInstance() {
+        if (instance == null) {
+            instance = new ChronoService();
+            System.out.println("Nueva instance");
+        }else{
+            System.out.println("Vieja instance");
+        }
+        return instance;
+    }
+}
